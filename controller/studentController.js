df48f2180db235a3752473bf9b5e004458a42591
darkling10 <pathan.abbas10@gmail.com>
2022-09-17 21:54:54 +0530

    Company Patch route added

--- controller/studentController.js
+++ controller/studentController.js
@@ -12,9 +12,9 @@ const studentProfileAdd = async (req, res) => {
   const authHeader = req.headers["x-access-token"];
   const token = authHeader && authHeader.split(" ")[1];
   const decoded = jwt.decode(token);
-
-  const student = await Students.findOne(decoded._id);
-  console.log(student);
+  console.log(decoded.id)
+  const student = await Students.findById(decoded.id);
+  // console.log(student);
 
   return res.status(200).json({ data: student });
 };
