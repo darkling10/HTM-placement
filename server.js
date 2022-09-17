559e98ad6ba5434dadbec8720a25ad2c13d6fdde
darkling10 <pathan.abbas10@gmail.com>
2022-09-17 21:46:07 +0530

    Company Patch route added

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const path = require("path")
+const path = require("path");
 let cors = require("cors");
-const bodyParser = require('body-parser')
-
+const bodyParser = require("body-parser");
 
 require("dotenv").config({
-  path:path.join(__dirname,".env")
-})
+  path: path.join(__dirname, ".env"),
+});
 
 //Database Connection
-require("./config/db")
-
+require("./config/db");
 
 //Middlewares
 app.use("/users", require("./routes/usersRoute"));
-app.use('/student',require('./routes/studentRoutes'))
-app.use('/company',require('./routes/companyRoutes'))
+app.use("/student", require("./routes/studentRoutes"));
+app.use("/company", require("./routes/companyRoutes"));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/",(req,res)=>{
-  res.send("Hello")
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 // Listening to the server
 app.listen(process.env.PORT, (req, res) => {
